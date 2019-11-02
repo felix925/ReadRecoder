dfea8edddbcc41a34c69d4aeee09908861f4c00c
felix925 <0wg32248278026y@ezweb.ne.jp>
2019-11-02 23:02:26 +0900

    Initial Commit

---
+++ ReadRecoder/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  ReadRecoder
+//
+//  Created by Nagahama Rei on 2019/11/02.
+//  Copyright © 2019 Nagahama Rei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
